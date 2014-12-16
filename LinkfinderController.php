479d88cdc39ea299d50e40d2d8a49579c30a840a
Sebastian Nuck <sebnuck@gmail.com>
2014-12-16 15:58:15 +0100

    Add communication logic with issn resolver

--- LinkfinderController.php
+++ LinkfinderController.php
@@ -11,8 +11,26 @@ class LinkfinderController extends OntoWiki_Controller_Component
     public function viewAction()
     {
         $_owApp = OntoWiki::getInstance();
+//        $modelIri = $this->_owApp->selectedModel->getModelIri();
         $logger = $_owApp->getCustomLogger("linkfinder");
-        $_owApp->callJob('IssnFinderJob', array('This is DATA!!!'));
+        $_owApp->callJob('IssnFinderJob', array('modelIri' => "http://ubl.amsl.technology/erm/"));
+
+//        $model = new Erfurt_Owl_Model($modelIri);
+//        $logger->debug('model:  ' . var_export($model));
+//        $sparql = '
+//            prefix bibrm: <http://vocab.ub.uni-leipzig.de/bibrm/>
+//            prefix umbel: <http://umbel.org/umbel#>
+//            SELECT ?issn WHERE {
+//                ?s a bibrm:ContractItem .
+//                ?s ?p ?issn .
+//                FILTER(regex(?p, "issn"))
+//                FILTER(regex(?issn, "urn:(issn|ISSN)"))
+//                #FILTER( NOT EXISTS {?issn umbel:isLike ?zdb })
+//            }';
+//
+//        //query selected model
+//        $query = Erfurt_Sparql_SimpleQuery::initWithString($sparql);
+//        $result = $model->sparqlQuery($sparql);
 
 
         $translate = $this->_owApp->translate;
--- jobs/IssnFinderJob.php
+++ jobs/IssnFinderJob.php
@@ -1,20 +1,147 @@
 <?php
 
 /**
- * This file is part of the {@link http://ontowiki.net OntoWiki} project.
+ * This file is part of the {@link http://amsl.technology amls} project.
  *
- * @copyright Copyright (c) 2014, {@link http://aksw.org AKSW}
+ * @copyright Copyright (c) 2014, {@link http://amsl.technology amsl}
  * @license http://opensource.org/licenses/gpl-license.php GNU General Public License (GPL)
  */
+
+//require_once realpath(dirname(__FILE__)) . '../classes/VerbalExpressions.php';
+
 class IssnFinderJob extends Erfurt_Worker_Job_Abstract
 {
 
-    public function run($load)
+    private $curl;
+    private $modelIri;
+    private $model;
+    private $_erfurt;
+
+    public function run($data)
     {
-        if (empty($load)) {
+        $this->logSuccess('preparing to start IssnFinderJob: ' . print_r($data, true));
+        $this->curl = curl_init();
+        $this->_erfurt = Erfurt_App::getInstance();
+        if (empty($data->modelIri)) {
             $this->logSuccess('started (without workload)');
         } else {
-            $this->logSuccess('IssnFinderJob started: ' . print_r($load, true));
+            $this->modelIri = $data->modelIri;
+            $this->logSuccess('IssnFinderJob started with Model IRI ' . $data->modelIri);
+            $this->model = new Erfurt_Owl_Model($data->modelIri);
+            $arrayOfIssn = $this->getIssn();
+            $this->logSuccess("nr of found issn: " . count($arrayOfIssn));
+            $looseIssn = $this->extractLooseIssn($arrayOfIssn);
+            $this->logSuccess('nr of proceeded issn: ' . count($looseIssn) . ' of ' . count($arrayOfIssn));
+        }
+        curl_close($this->curl);
+    }
+
+    /*
+     * Retrieve all ISSN that
+     */
+    private function getIssn()
+    {
+        $sparql = '
+            prefix bibrm: <http://vocab.ub.uni-leipzig.de/bibrm/>
+            prefix umbel: <http://umbel.org/umbel#>
+            SELECT ?issn WHERE {
+                ?s a bibrm:ContractItem .
+                ?s ?p ?issn .
+                FILTER(regex(?p, "issn"))
+                FILTER(regex(?issn, "urn:(issn|ISSN)"))
+                #FILTER (NOT EXISTS {
+                #    ?issn umbel:isLike ?zdb .
+                #})
+            } LIMIT 10';
+
+        //query selected model
+        $result = $this->model->sparqlQuery($sparql);
+        return $result;
+    }
+
+    private function extractLooseIssn($results)
+    {
+        // array to store the loose issn
+        $return = array();
+
+        foreach ($results as $arrayOfIssn) {
+            $issn = $arrayOfIssn['issn'];
+            $sparql = "
+            prefix umbel: <http://umbel.org/umbel#>
+            SELECT ?zdb WHERE {
+                <$issn> umbel:isLike ?zdb .
+            }";
+            $result = $this->model->sparqlQuery($sparql);
+
+            // if ask query returns nothing, its result is false (OntoWiki specific)
+            if (empty($result)) {
+                $this->logSuccess("issn: " . $issn);
+                $answer = $this->askIssnResolver(substr($issn, -9));
+                $this->_import($answer);
+                $return[] = $issn;
+            } else {
+                $this->logSuccess('result not empty, resource already imported: ' . print_r($result, true));
+            }
+        }
+
+        return $return;
+
+    }
+
+    private function askIssnResolver($issn)
+    {
+        $url = "http://data.ub.uni-leipzig.de/zdb/issn/$issn";
+        $this->logSuccess('asking issn resolver for: ' . $issn);
+
+        curl_setopt($this->curl, CURLOPT_URL, $url);
+        curl_setopt($this->curl, CURLOPT_CUSTOMREQUEST, 'GET');
+        curl_setopt($this->curl, CURLOPT_HEADER, 0);
+        curl_setopt($this->curl, CURLOPT_FRESH_CONNECT, true);
+        curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($this->curl, CURLOPT_TIMEOUT, 30);
+        $answer = curl_exec($this->curl);
+        return $answer;
+    }
+
+
+    private function _import($data)
+    {
+        // action spec for versioning
+        $versioning = $this->_erfurt->getVersioning();
+        $actionSpec                = array();
+        $actionSpec['type']        = 11;
+        $actionSpec['modeluri']    = $this->modelIri;
+        $actionSpec['resourceuri'] = $this->modelIri;
+
+        // since the importer has problems with urn:issn:xxxx-xxxx uris,
+        // we have to define the base as urn:issn:
+        $dataWithBase = '@base <urn:ISSN:> . ';
+        $dataWithBase .= $data;
+
+        try {
+            // starting versioning action
+            $versioning->startAction($actionSpec);
+
+            $locator = Erfurt_Syntax_RdfParser::LOCATOR_DATASTRING;
+            $filetype = 'ttl';
+            $this->logSuccess('trying to import modelIRI ' . $this->modelIri . ' and data: ' . $dataWithBase);
+            $this->_erfurt->getStore()->importRdf($this->modelIri, $data, $filetype, $locator);
+            $this->logSuccess('finished importing');
+            // stopping versioning action
+            $versioning->endAction();
+
+            // Trigger Reindex
+//            $indexEvent = new Erfurt_Event('onFullreindexAction');
+//            $indexEvent->trigger();
+
+        } catch (Erfurt_Exception $e) {
+            // re-throw
+            throw new OntoWiki_Controller_Exception(
+                'Could not import given model: ' . $e->getMessage(),
+                0,
+                $e
+            );
         }
     }
+
 }
